c2db9c440e2afecd46d9417893443de40934c1f4
liuqichen <liuqichne@gmail.com>
2023-06-10 03:35:17 +0200

    Initial Commit

---
+++ asciibar/asciibarApp.swift
@@ -0,0 +1,17 @@
+//
+//  asciibarApp.swift
+//  asciibar
+//
+//  Created by 刘启辰 on 2023/6/10.
+//
+
+import SwiftUI
+
+@main
+struct asciibarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
